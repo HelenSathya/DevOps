1e108024800d8bab430bb49e60e5f871c4d3cb25
HelenSathya <127550125+HelenSathya@users.noreply.github.com>
2023-03-11 01:56:22 +0530

    Devops assignment files

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\helsy\BITS\Softwares\flutter_windows_3.3.10-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\helsy\BITS\Semester3\CPAD - Cross_Platform\Assignment\Final_deliverables\assignment_g7_cpa\assignment_g7_cpa"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
